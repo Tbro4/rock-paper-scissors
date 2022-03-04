8bd6fbd0cd74285e76b07c25b8b71351d8bca2f0
Tyler Brooks <thetylerbrooks@gmail.com>
2022-03-03 21:55:24 -0700

    working and looping, need an option for incorrect entries

--- assets/app.js
+++ assets/app.js
@@ -3,67 +3,79 @@ var loses = 0;
 var ties = 0;
 var compOptions = ["R", "P", "S"];
 
-var userChoice = prompt("Please enter R(rock), P(paper), or S(scissors)");
-console.log(userChoice);
+function start() {
+  var userChoice = prompt("Please enter R(rock), P(paper), or S(scissors)");
+  console.log(userChoice);
 
-var cleanChoice = userChoice.toUpperCase();
-console.log(cleanChoice);
+  var cleanChoice = userChoice.toUpperCase();
+  console.log(cleanChoice);
 
-var compChoice = Math.floor(Math.random() * compOptions.length);
-console.log(compOptions[compChoice]);
+  var compChoice = Math.floor(Math.random() * compOptions.length);
+  console.log(compOptions[compChoice]);
 
-if (cleanChoice === compOptions[compChoice]) {
-  alert(
-    "The computer also chose " + compOptions[compChoice] + "." + " It's a tie!"
-  );
-  ties++;
-} else if (cleanChoice === "R") {
-  if (compOptions[compChoice] === "S") {
+  if (cleanChoice === compOptions[compChoice]) {
     alert(
-      "The computer chose " +
+      "The computer also chose " +
         compOptions[compChoice] +
         "." +
-        " 🎉🎉 You win! 🎉🎉"
+        " It's a tie!"
     );
-    wins++;
-  } else {
-    alert(
-      "The computer chose " + compOptions[compChoice] + "." + " You lose! 😥"
-    );
-    loses++;
+    ties++;
+  } else if (cleanChoice === "R") {
+    if (compOptions[compChoice] === "S") {
+      alert(
+        "The computer chose " +
+          compOptions[compChoice] +
+          "." +
+          " 🎉🎉 You win! 🎉🎉"
+      );
+      wins++;
+    } else {
+      alert(
+        "The computer chose " + compOptions[compChoice] + "." + " You lose! 😥"
+      );
+      loses++;
+    }
+  } else if (cleanChoice === "P") {
+    if (compOptions[compChoice] === "R") {
+      alert(
+        "The computer chose " +
+          compOptions[compChoice] +
+          "." +
+          " 🎉🎉 You win! 🎉🎉"
+      );
+      wins++;
+    } else {
+      alert(
+        "The computer chose " + compOptions[compChoice] + "." + " You lose! 😥"
+      );
+      loses++;
+    }
+  } else if (cleanChoice === "S") {
+    if (compOptions[compChoice] === "P") {
+      alert(
+        "The computer chose " +
+          compOptions[compChoice] +
+          "." +
+          " 🎉🎉 You win! 🎉🎉"
+      );
+      wins++;
+    } else {
+      alert(
+        "The computer chose " + compOptions[compChoice] + "." + " You lose! 😥"
+      );
+      loses++;
+    }
   }
-} else if (cleanChoice === "P") {
-  if (compOptions[compChoice] === "R") {
-    alert(
-      "The computer chose " +
-        compOptions[compChoice] +
-        "." +
-        " 🎉🎉 You win! 🎉🎉"
-    );
-    wins++;
+  alert("Wins: " + wins + "Loses: " + loses + "Ties: " + ties);
+
+  var playAgain = confirm("Would you like to play again?");
+  console.log(playAgain);
+
+  if (playAgain) {
+    start();
   } else {
-    alert(
-      "The computer chose " + compOptions[compChoice] + "." + " You lose! 😥"
-    );
-    loses++;
-  }
-} else if (cleanChoice === "S") {
-  if (compOptions[compChoice] === "P") {
-    alert(
-      "The computer chose " +
-        compOptions[compChoice] +
-        "." +
-        " 🎉🎉 You win! 🎉🎉"
-    );
-    wins++;
-  } else {
-    alert(
-      "The computer chose " + compOptions[compChoice] + "." + " You lose! 😥"
-    );
-    loses++;
+    alert("Thanks for playing!");
   }
 }
-alert("Wins: " + wins + "Loses: " + loses + "Ties: " + ties);
-
-var playAgain = confirm("Would you like to play again?");
-console.log(playAgain);
+start();
